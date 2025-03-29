31dc75893f7b41e4cd56a991e774707b7ccab957
wassemsoodan <98319634+WasimSoudan@users.noreply.github.com>
2025-03-29 15:48:11 +0200

    git init

---
+++ src/components/About/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage2.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem} >
+            <img src={getImageUrl("about/education3.jpg")} alt="Cursor icon" style={{ marginRight: "5px" }} />
+            <div className={styles.aboutItemText}>
+              <h3>Education</h3>
+              <p>Master in Computer Science</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer skilled in creating responsive and
+                high-performance websites.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience building efficient and optimized backend
+                systems and APIs.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
